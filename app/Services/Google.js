05d93f59cd28800597ae9706d6ce7c8540572c79
brian3t <ngxtri@gmail.com>
2020-05-17 21:30:48 -0700

    use another free google API (1)

--- app/Services/Google.js
+++ app/Services/Google.js
@@ -2,7 +2,8 @@
 const {google} = require('googleapis')
 // const YOUTUBE_PK='AIzaSyDthANcEwY313X8P27E1SE_CHvq3M_f0CE' //ngxtri@gmail.com
 // const YOUTUBE_PK='AIzaSyD4aB9qEEHdmkYe5rX5INjU3UxkawaiUyo' //someids@gmail.com
-const YOUTUBE_PK='AIzaSyCw_FAt-qYU7r-uSGZ9Rg-SMgz6LpOsY2w' //brian3t0512
+// const YOUTUBE_PK='AIzaSyCw_FAt-qYU7r-uSGZ9Rg-SMgz6LpOsY2w' //brian3t0512
+const YOUTUBE_PK='AIzaSyCtldbHl9WoT5Eu9dv_svGmpbpnXxOeP7Q' //brian3t0513
 
 class Google {
   /**
